06c9a50ca1df0edac469388ee020064516cae81b
Joe Patterson <Joe Patterson>
2016-10-20 22:57:03 +1000

    Drawer complete

--- src/sagas/game.ts
+++ src/sagas/game.ts
@@ -18,7 +18,7 @@ import {
 
 
 function getWinner(gameBoard: Token[][]): Token {
-    
+
     let winnerToken: Token;
 
     const verticalWinner = _.chain(gameBoard)
@@ -31,10 +31,10 @@ function getWinner(gameBoard: Token[][]): Token {
     }
 
     let flippedGameBoardPreZipp: Token[][] = _.cloneDeep(gameBoard);
-    let flippedGameBoard: Token[][] = _.unzip(gameBoard)
+    let flippedGameBoard: Token[][] = _.unzip(flippedGameBoardPreZipp);
 
-    if(!winnerToken){
-        winnerToken = getVertical(flippedGameBoard)
+    if (!winnerToken) {
+        winnerToken = getHoriz(flippedGameBoard);
     }
 
 
@@ -43,15 +43,13 @@ function getWinner(gameBoard: Token[][]): Token {
     }
 
     if (!winnerToken) {
-        let reverseGameBoard: Token[][] = _.cloneDeep(gameBoard)
-        _.reverse(reverseGameBoard)
-        winnerToken = getDiagonalToken(reverseGameBoard)
+        winnerToken =  getDiagonalToken(gameBoard, true);
     }
 
-    if(!winnerToken){
-        
+    if (!winnerToken && isDraw(flippedGameBoardPreZipp)) {
+        return Token.Empty;
     }
-
+//return Token.Empty; 
 
     if (winnerToken) {
         return winnerToken;
@@ -60,27 +58,38 @@ function getWinner(gameBoard: Token[][]): Token {
     return null;
 }
 
-function getDiagonalToken(flippedGameBoard: Token[][]): any {
-     for (let i = 0; i < 2; i++) {
-         let base = flippedGameBoard[i][0];
-         let basePlus1 = base === flippedGameBoard[i + 1][1];
-         let basePlus2 = base === flippedGameBoard[i + 2][2];
-         let basePlus3 = base === flippedGameBoard[i + 3][3];
+function isDraw(grid: Token[][]) {
+    return !grid.some(g=> g.find(a=>a===Token.Empty)>-1)
+}
+
+function getDiagonalToken(flippedGameBoard: Token[][], reverse = false): any {
+    for (let i = 0; i < 3; i++) {
+        let base = flippedGameBoard[i][0];
+        let offset = [1, 2, 3];
+        if (reverse) {
+            base = flippedGameBoard[i][3];
+            offset = [2, 1, 0];
+        }
+
+        let basePlus1 = base === flippedGameBoard[i + 1][offset[0]];
+        let basePlus2 = base === flippedGameBoard[i + 2][offset[1]];
+        let basePlus3 = base === flippedGameBoard[i + 3][offset[2]];
         if (basePlus1 && basePlus2 && basePlus3 && base != Token.Empty) {
             return base;
         }
-     }
+    }
 }
 
-function getVertical(flippedGameBoard: Token[][]):Token{
+function getHoriz(flippedGameBoard: Token[][]): Token {
     let winningToken: Token;
     flippedGameBoard.forEach(row => {
         let count = 0;
-        let lastToken = row[0];
+        let lastToken = Token.Empty;
         row.forEach(quickToken => {
             if (!winningToken && quickToken !== Token.Empty && quickToken === lastToken) {
-                if (count === 3) {
+                if (count === 2) {
                     winningToken = quickToken;
+                    return;
                 } else {
                     count++;
                 }
@@ -90,14 +99,14 @@ function getVertical(flippedGameBoard: Token[][]):Token{
             lastToken = quickToken;
         });
     });
-    return winningToken
+    return winningToken;
 }
 
 export default function* game(): any {
     while (yield take(PLACE_TOKEN)) {
         const gameBoard = yield select < Store > (s => s.gameBoard);
         const winner = getWinner(gameBoard);
-        if (winner) {
+        if (winner!==null && winner>-1) {
             yield put(gameOver(winner));
             break;
         }
--- test/sagas/game.ts
+++ test/sagas/game.ts
@@ -1,9 +1,15 @@
 import testSaga from "redux-saga-test-plan";
 import * as test from "tape";
 
-import {PLACE_TOKEN, gameOver, placeToken} from "actions/game";
+import {
+    PLACE_TOKEN,
+    gameOver,
+    placeToken
+} from "actions/game";
 import game from "sagas/game";
-import {Token} from "store/store";
+import {
+    Token
+} from "store/store";
 
 test("Vertical win", t => {
     const saga = testSaga(game);
@@ -43,6 +49,45 @@ test("Horizontal win", t => {
     t.end();
 });
 
+test("Horizontal win middle", t => {
+    const saga = testSaga(game);
+
+    const board = [
+        [Token.Empty, Token.Empty, Token.Empty, Token.Empty],
+        [Token.Red, Token.Empty, Token.Empty, Token.Empty],
+        [Token.Red, Token.Empty, Token.Empty, Token.Empty],
+        [Token.Red, Token.Empty, Token.Empty, Token.Empty],
+        [Token.Red, Token.Empty, Token.Empty, Token.Empty],
+        [Token.Empty, Token.Empty, Token.Empty, Token.Empty],
+    ];
+
+    saga.next().take(PLACE_TOKEN)
+        .next(placeToken(0))
+        .next(board).put(gameOver(Token.Red));
+
+    t.end();
+});
+
+
+test("Horizontal win end", t => {
+    const saga = testSaga(game);
+
+    const board = [
+        [Token.Empty, Token.Empty, Token.Empty, Token.Empty],
+        [Token.Empty, Token.Empty, Token.Empty, Token.Empty],
+        [Token.Red, Token.Empty, Token.Empty, Token.Empty],
+        [Token.Red, Token.Empty, Token.Empty, Token.Empty],
+        [Token.Red, Token.Empty, Token.Empty, Token.Empty],
+        [Token.Red, Token.Empty, Token.Empty, Token.Empty],
+    ];
+
+    saga.next().take(PLACE_TOKEN)
+        .next(placeToken(0))
+        .next(board).put(gameOver(Token.Red));
+
+    t.end();
+});
+
 test("Diagonal win", t => {
     const saga = testSaga(game);
 
@@ -58,6 +103,44 @@ test("Diagonal win", t => {
     saga.next().take(PLACE_TOKEN)
         .next(placeToken(0))
         .next(board).put(gameOver(Token.Red));
+
+    t.end();
+});
+
+test("Diagonal win oposite", t => {
+    const saga = testSaga(game);
+
+    const board = [
+        [Token.Yellow, Token.Yellow, Token.Yellow, Token.Red],
+        [Token.Red, Token.Red, Token.Red, Token.Empty],
+        [Token.Yellow, Token.Red, Token.Empty, Token.Empty],
+        [Token.Red, Token.Empty, Token.Empty, Token.Empty],
+        [Token.Empty, Token.Empty, Token.Empty, Token.Empty],
+        [Token.Empty, Token.Empty, Token.Empty, Token.Empty],
+    ];
+
+    saga.next().take(PLACE_TOKEN)
+        .next(placeToken(0))
+        .next(board).put(gameOver(Token.Red));
+
+    t.end();
+});
+
+test("Drawer", t => {
+    const saga = testSaga(game);
+
+    const board = [
+        [Token.Yellow, Token.Red, Token.Yellow, Token.Yellow],
+        [Token.Red, Token.Red, Token.Red, Token.Yellow],
+        [Token.Yellow, Token.Red, Token.Red, Token.Red],
+        [Token.Red, Token.Yellow, Token.Yellow, Token.Yellow],
+        [Token.Yellow, Token.Red, Token.Red, Token.Yellow],
+        [Token.Red, Token.Yellow, Token.Yellow, Token.Yellow]
+    ];
+
+    saga.next().take(PLACE_TOKEN)
+        .next(placeToken(2))
+        .next(board).put(gameOver(Token.Empty));
 
     t.end();
 });
